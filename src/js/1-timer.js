54aea3bf18864ca0c42f8e0c93d83f73906d7b09
Oleksandr Kliuiko <oleksandr.kliuiko5804@gmail.com>
2026-09-07 00:39:29 +0300

    1-timer complete

---
+++ src/js/1-timer.js
@@ -0,0 +1,80 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const btn = document.querySelector('button');
+const picker = document.querySelector('#datetime-picker');
+
+let userSelectedDate = new Date();
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      console.log(new Date());
+
+      if (selectedDates[0] <= new Date()) {
+          btn.disabled = true;
+          window.alert("Please choose a date in the future");
+      } else {
+          btn.disabled = false;
+          userSelectedDate = selectedDates[0];
+      }
+  },
+};
+
+flatpickr("#datetime-picker", options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+function updateTimer() {
+    const difference = userSelectedDate - (new Date());
+
+    if (difference <= 0) {
+        clearInterval(intervalID);
+        btn.disabled = false;
+        picker.disabled = false;
+        return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(difference);
+
+     daysEl.textContent = String(days).padStart(2, '0');
+    hoursEl.textContent = String(hours).padStart(2, '0');
+    minutesEl.textContent = String(minutes).padStart(2, '0');
+    secondsEl.textContent = String(seconds).padStart(2, '0');
+
+};
+
+let intervalID;
+
+btn.addEventListener("click", () => {
+    btn.disabled = true;
+    picker.disabled = true;
+    intervalID = setInterval(updateTimer, 1000);
+})
